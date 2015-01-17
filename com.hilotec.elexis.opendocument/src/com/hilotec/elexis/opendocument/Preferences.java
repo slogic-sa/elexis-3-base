2284aef99739b7a2af00b17ab49427f1d0ea8bdd
Niklaus Giger <niklaus.giger@member.fsf.org>
2015-01-17 14:13:45 +0100

    [3229,3231]Copied all changes from master branch into release for c.h.e.opendocument

--- com.hilotec.elexis.opendocument/src/com/hilotec/elexis/opendocument/Preferences.java
+++ com.hilotec.elexis.opendocument/src/com/hilotec/elexis/opendocument/Preferences.java
@@ -24,34 +24,41 @@ import ch.elexis.core.ui.preferences.SettingsPreferenceStore;
 import ch.elexis.core.ui.preferences.inputs.MultilineFieldEditor;
 
 public class Preferences extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
-
+	
 	private static final String BASE = "com.hilotec.elexis.ooo.";
 	public static final String P_EDITOR = BASE + "editorpath";
 	public static final String P_EDITARGS = BASE + "editargs";
 	public static final String P_PRINTARGS = BASE + "printargs";
-	public static final String P_ONLY_ONE_OPEN = BASE + "only_one_open";
-
+	public static final String P_PDFCONVERTER = BASE + "pdfpath";
+	public static final String P_PDFARGS = BASE + "pdfargs";
+	public static final String P_WRAPPERSCRIPT = BASE + "wrapperscript";
+	
 	SettingsPreferenceStore prefs = new SettingsPreferenceStore(CoreHub.localCfg);
-
+	
 	public Preferences(){
 		super(GRID);
 		setPreferenceStore(prefs);
 		setDescription("Einstellungen für das Hilotec-OpenDocument-Plugin");
 	}
-
+	
 	@Override
 	protected void createFieldEditors(){
 		addField(new FileFieldEditor(P_EDITOR, "OpenDocument Editor", getFieldEditorParent()));
 		addField(new MultilineFieldEditor(P_EDITARGS, "Argumente zum Editieren", 5, SWT.V_SCROLL,
 			true, getFieldEditorParent()));
-		addField(new BooleanFieldEditor(P_ONLY_ONE_OPEN, "Nur eine offene Datei erlauben",
+		addField(new MultilineFieldEditor(P_PRINTARGS, "Argumente zum Drucken", 5, SWT.V_SCROLL,
+			true, getFieldEditorParent()));
+		addField(new FileFieldEditor(P_PDFCONVERTER, "PDF-Konverter", getFieldEditorParent()));
+		addField(new MultilineFieldEditor(P_PDFARGS, "Argumente fuer PDF-Konvertierung", 5,
+			SWT.V_SCROLL, false, getFieldEditorParent()));
+		addField(new BooleanFieldEditor(P_WRAPPERSCRIPT, "Wrapper Skript aktivieren",
 			getFieldEditorParent()));
 	}
-
+	
 	@Override
 	public void init(IWorkbench arg0){
 		// TODO Auto-generated method stub
-
+		
 	}
-
+	
 }
